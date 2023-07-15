b6d932b0c8d7b990f6884af4670df6669fd44e2f
vincentanu04 <vincentanu04@gmail.com>
2023-07-16 04:37:46 +0800

    Finish turning form to cards

--- build/js/main.js
+++ build/js/main.js
@@ -2,7 +2,13 @@ const initApp = () => {
     const openButtons = document.querySelectorAll(".add-book-button");
     const cancelButton = document.getElementById("cancel");
     const submitButton = document.querySelector("[type='submit']");
+
+    const formSection = document.getElementById("book-section");
     const bookForm = document.getElementById("book-form");
+    const titleInput = document.getElementById("title");
+    const authorInput = document.getElementById("author");
+    const pagesInput = document.getElementById("pages");
+    const readInput = document.getElementById("read");
     const myLibrary = [];
 
     function Book(title, author, pages, readOrNot) {
@@ -20,26 +26,67 @@ const initApp = () => {
     }
 
     function toggleForm() {
-        bookForm.classList.toggle("hidden");
-        bookForm.classList.toggle("grid");
+        formSection.classList.toggle("hidden");
+        formSection.classList.toggle("grid");
+    }
+
+    function displayBooks(myLibrary) {
+        myLibrary.forEach((book) => {
+            let templateClone = document.getElementById("template").cloneNode(true);
+            templateClone.querySelector("#title-template").textContent = book.title;
+            templateClone.querySelector("#author-template").textContent = `by ${book.author}`;
+            templateClone.querySelector("#pages-template").textContent = `${book.pages} pages`;
+            templateClone.querySelector("#read-template").textContent = `Read: ${book.readOrNot}`;
+            templateClone.classList.remove("hidden");
+            templateClone.classList.add("flex");
+            document.getElementById("books").appendChild(templateClone);
+        });
     }
 
     openButtons.forEach((button) => {
         button.addEventListener("click", toggleForm);
     });
 
-    bookForm.addEventListener("click", (e) => {
-        if (e.target === bookForm) {
+    formSection.addEventListener("click", (e) => {
+        if (e.target === formSection) {
             toggleForm();
         }
     });
 
-    cancelButton.addEventListener("click", toggleForm)
+    cancelButton.addEventListener("click", toggleForm);
     
     submitButton.addEventListener("click", (e) => {
         e.preventDefault();
+        const formInputs = [titleInput, authorInput, pagesInput, readInput];
+        let atLeastOne = false;
+        formInputs.forEach(input => { /* Maybe should use for of */
+            if (input.value.trim() === "") {
+                if(!input.classList.contains("border-red-600")) {
+                    input.classList.toggle("border-theme-secondary");
+                    input.classList.toggle("border-red-600");
+                }
+                atLeastOne = true;
+            }
+            else {
+                if(input.classList.contains("border-red-600")) {
+                    input.classList.toggle("border-theme-secondary");
+                    input.classList.toggle("border-red-600");
+                };
+            }
+        });
+        if (atLeastOne) return;
+        const newBook = new Book(
+            titleInput.value,
+            authorInput.value,
+            pagesInput.value,
+            readInput.value);
+
+        bookForm.reset();
+        document.getElementById("books").innerHTML = "";
+        addBookToLibrary(newBook);
+        displayBooks(myLibrary);
         toggleForm();
-    })
+    });
 }
 
 document.addEventListener("DOMContentLoaded", initApp);
